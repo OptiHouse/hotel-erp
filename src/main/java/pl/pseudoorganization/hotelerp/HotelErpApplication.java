02bbb65f6ff7646ddf91f4265f66abeab0a8f8eb
Dawid Szafka <szafkadawid001@gmail.com>
2023-12-03 15:07:20 +0100

    feat: initial commit

---
+++ src/main/java/pl/pseudoorganization/hotelerp/HotelErpApplication.java
@@ -0,0 +1,13 @@
+package pl.pseudoorganization.hotelerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pseudoorganization/hotelerp/HotelErpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pseudoorganization.hotelerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
