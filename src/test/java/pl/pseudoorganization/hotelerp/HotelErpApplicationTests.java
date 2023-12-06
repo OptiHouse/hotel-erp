15f55c759d2c995a416152e0b955ecf5f46d65a7
Dawid Szafka <szafkadawid001@gmail.com>
2023-12-06 10:08:04 +0100

    feature: add integration tests for Employee

--- src/test/java/pl/pseudoorganization/hotelerp/HotelErpApplicationTests.java
+++ src/test/java/pl/pseudoorganization/hotelerp/HotelErpApplicationTests.java
@@ -12,7 +12,11 @@ import pl.pseudoorganization.hotelerp.model.chore.Chore;
 import pl.pseudoorganization.hotelerp.model.chore.ChoreRequest;
 import pl.pseudoorganization.hotelerp.model.chore.ChoreStatus;
 import pl.pseudoorganization.hotelerp.model.chore.ChoreType;
+import pl.pseudoorganization.hotelerp.model.employee.Employee;
+import pl.pseudoorganization.hotelerp.model.employee.EmployeeRequest;
+import pl.pseudoorganization.hotelerp.model.employee.EmployeeRole;
 import pl.pseudoorganization.hotelerp.repository.ChoreRepository;
+import pl.pseudoorganization.hotelerp.repository.EmployeeRepository;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,12 +36,15 @@ class HotelErpApplicationTests {
     private MockMvc mvc;
 
     @Autowired
-    private ChoreRepository repository;
+    private ChoreRepository choreRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    public void whenValidInput_thenCreateChore() throws Exception {
+    void whenValidInput_thenCreateChore() throws Exception {
         var choreRequest = ChoreRequest.builder()
                 .title("Test")
                 .description("Test")
@@ -63,7 +70,7 @@ class HotelErpApplicationTests {
     }
 
     @Test
-    public void shouldGetChores() throws Exception {
+    void shouldGetChores() throws Exception {
         var chore = Chore.builder()
                 .title("Test")
                 .description("Test")
@@ -73,7 +80,7 @@ class HotelErpApplicationTests {
                 .createdBy(1L)
                 .build();
         var chores = List.of(chore);
-        repository.save(chore);
+        choreRepository.save(chore);
 
         var response = mvc.perform(get("/api/chores"))
                 .andExpect(status().isOk())
@@ -82,5 +89,50 @@ class HotelErpApplicationTests {
         var result = Arrays.asList(objectMapper.readValue(response, Chore[].class));
 
         assertThat(result).usingRecursiveComparison().isEqualTo(chores);
+    }
+
+    @Test
+    void whenValidInput_thenCreateEmployee() throws Exception {
+        var employeeRequest = EmployeeRequest.builder()
+                .firstName("Artur")
+                .lastName("Kowalski")
+                .email("Artur.kowalski@example.com")
+                .password("password")
+                .role(EmployeeRole.MANAGER.getCode())
+                .build();
+
+        var response = mvc.perform(post("/api/employee")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(employeeRequest)))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        var result = objectMapper.readValue(response, Employee.class);
+
+        assertEquals(employeeRequest.getFirstName(), result.getFirstName());
+        assertEquals(employeeRequest.getLastName(), result.getLastName());
+        assertEquals(employeeRequest.getEmail(), result.getEmail());
+        assertEquals(employeeRequest.getRole(), result.getRole().getCode());
+    }
+
+    @Test
+    void shouldGetEmployees() throws Exception {
+        var employee = Employee.builder()
+                .firstName("Jan")
+                .lastName("Kowalski")
+                .email("jan.kowalski@example.com")
+                .password("password")
+                .role(EmployeeRole.MANAGER)
+                .build();
+        var employees = List.of(employee);
+        employeeRepository.save(employee);
+
+        var response = mvc.perform(get("/api/employee"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        var result = Arrays.asList(objectMapper.readValue(response, Employee[].class));
+
+        assertThat(result).usingRecursiveComparison().isEqualTo(employees);
     }
 }
