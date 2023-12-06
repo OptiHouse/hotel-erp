54b1db1f2ef048282ba686c385601650f120e94e
Dawid Szafka <szafkadawid001@gmail.com>
2023-12-06 13:42:02 +0100

    feat: add service for empolyee

--- src/main/java/pl/pseudoorganization/hotelerp/controller/EmployeeController.java
+++ src/main/java/pl/pseudoorganization/hotelerp/controller/EmployeeController.java
@@ -1,60 +1,39 @@
 package pl.pseudoorganization.hotelerp.controller;
 
-import io.micrometer.common.util.StringUtils;
 import jakarta.annotation.Nullable;
 import org.springframework.web.bind.annotation.*;
 import pl.pseudoorganization.hotelerp.error.ApplicationException;
-import pl.pseudoorganization.hotelerp.error.ErrorCode;
 import pl.pseudoorganization.hotelerp.model.employee.Employee;
 import pl.pseudoorganization.hotelerp.model.employee.EmployeeRequest;
-import pl.pseudoorganization.hotelerp.model.employee.EmployeeRole;
-import pl.pseudoorganization.hotelerp.repository.EmployeeRepository;
+import pl.pseudoorganization.hotelerp.services.EmployeeService;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/employee")
 public class EmployeeController {
 
-    EmployeeRepository employeeRepository;
+    EmployeeService employeeService;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping()
     public List<Employee> getAllEmployees(@RequestParam(required = false) @Nullable String lastName,
                                                    @RequestParam(required = false) @Nullable String role) throws ApplicationException {
-        return getEmployeesByParam(lastName, role);
+        return employeeService.getAllEmployees(lastName, role);
     }
 
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable("id") String id) throws ApplicationException {
-        return employeeRepository.findById(Long.parseLong(id))
-                .orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND));
+        return employeeService.getEmployeeById(id);
     }
 
     @PostMapping()
     public Employee addEmployee(@RequestBody EmployeeRequest employeeRequest) throws ApplicationException {
-        Optional<Employee> existingEmployee = employeeRepository.findByFirstNameAndLastName(employeeRequest.getFirstName(), employeeRequest.getLastName());
-        if (existingEmployee.isPresent()) {
-            throw new ApplicationException(ErrorCode.USER_ALREADY_EXISTS);
-        }
-        Employee employee = new Employee(employeeRequest);
-        employeeRepository.save(employee);
-        return employee;
+        return employeeService.addEmployee(employeeRequest);
     }
 
-    private List<Employee> getEmployeesByParam(String lastName, String role) throws ApplicationException {
-        if (StringUtils.isNotBlank(role)) {
-            return employeeRepository.findByRole(EmployeeRole.createByCode(role))
-                    .orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND));
-        }
-        if (StringUtils.isNotBlank(lastName)) {
-            return (employeeRepository.findByLastName(lastName))
-                    .orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND));
-        }
-        return employeeRepository.findAll();
-    }
+
 }
